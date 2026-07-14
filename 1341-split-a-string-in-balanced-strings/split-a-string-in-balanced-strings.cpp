645a39c3a140525fcf16854dea425c6964fc39a9
Gaurav  Singh <gaurav9559singh@gmail.com>
2026-07-14 22:06:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (57.09%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int count = 0;
+        int balance = 0;
+        
+        for (char c : s) {
+            if (c == 'R') {
+                balance++;
+            } else {
+                balance--;
+            }
+            
+            if (balance == 0) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};
